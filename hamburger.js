23fc44a326dae583236fcdbd0e809f4fa0f59492
P1wP <jokke90@gmail.com>
2019-05-29 21:47:18 +0200

    First commit Menu work in progress

---
+++ hamburger.js
@@ -0,0 +1,18 @@
+// Open close menu
+
+// Get hamburger Icon
+var open = document.getElementById("hamburger");
+// Get X icon
+var close = document.getElementById("closeNav");
+// Get navigation list
+var navigation = document.getElementById("navigation");
+
+
+/* Change position and icon on click */
+open.addEventListener("click", function(){
+     navigation.style.left = "0px";
+})
+
+close.addEventListener("click", function(){
+    navigation.style.left ="-300px";
+})
